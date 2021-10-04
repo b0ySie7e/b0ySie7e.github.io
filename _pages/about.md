ecd13d20bfe797d8341b3805b6cc2a17b7c3d653
b0ySie7e <Zombi1532ao@gmail.com>
2021-10-03 21:29:45 -0400

    Actualizacion

---
+++ _pages/about.md
@@ -0,0 +1,18 @@
+---
+layout: about
+title: "About"
+permalink: /about/
+date: 2021-09-17
+clases: wide
+header:
+    teaser_home_page: true
+---
+## <center>INFORMACION PERSONAL</center>
+
+- Hola, soy b0ySie7e. Este es el nickname con el que se me conoce en la red, en realidad mi nombre verdadero es ```Roland``` Soy un entusiasta y amante de la informatica y todo lo que tenga que ver con la tecnologia de la actualidad y la del futuro, en especial por el campo de la ciberseguridad el cual me atrae y me encantaria dedicarme a esto profesionalmente. En cual estoy adquiriendo nuevos conocimientos de manera autodidacta.
+
+- En este blog encontraras resoluciones de maquinas de [Tryhackme](https://tryhackme.com/) y de [Hakcthebox](https://www.hackthebox.eu/) y muchas informacion(notas) del conocimiento de vaya adquiriendo, asi mismo informacion de herramientas que son utiles, basicamente recursos orientados a la ciberseguridad
+    
+- - -
+
+<br>" La Informática y la gente son las cosas que me mantienen vivo ```Que comience el hacking```  "
---
+++ _site/assets/js/lunr/lunr-store.js
@@ -0,0 +1,11 @@
+var store = [{
+        "title": "Nmap - Herramienta de reconocimiento",
+        "excerpt":"¿Qué es nmap? Network Mapped (Nmap) es una herramienta de escaneo de red y detección de host. Se usa para el reconocimiento y la recopilacion información sobre una red determinada. Una herramienta muy potente a la hora de pruebas de penetracion. Nmap no se limita a la mera recopilación y...","categories": ["herramientas"],
+        "tags": ["linux"],
+        "url": "http://localhost:4000/Nmap/",
+        "teaser":null},{
+        "title": "Mustacchio- Tryhackme",
+        "excerpt":"RECONOCIMIENTO Usamos -sS --min-rate 5000 para ir mucho mas rapido con el escaneo, luego lo guardaremos en el archivo allportsScan. Luego del escaneo se tendra el siguiente resultado. Sie7e$ nmap -p- --open -sS --min-rate 5000 -Pn -vvv -oG allportsScan &lt;&lt;IP target&gt;&gt; # Ports scanned: TCP(65535;1-65535) UDP(0;) SCTP(0;) PROTOCOLS(0;) Host: &lt;IP...","categories": ["tryhackme"],
+        "tags": ["linux","write up","xxe","easy"],
+        "url": "http://localhost:4000/thm-Mustacchio/",
+        "teaser":null}]
